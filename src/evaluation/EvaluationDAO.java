2775f0d4b04f11a600dd58d446ceabf91aeb7f14
operasystem6 <operasystem6@naver.com>
2020-11-18 20:31:18 +0900

    검색하기 기능 구현

--- src/evaluation/EvaluationDAO.java
+++ src/evaluation/EvaluationDAO.java
@@ -3,6 +3,7 @@ package evaluation;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import util.DatabaseUtil;
 
@@ -47,5 +48,68 @@ public class EvaluationDAO {
 			}
 		}
 		return -1; 
+	}
+	
+	public ArrayList<EvaluationDTO> getList(String genre, String searchType, String search, int pageNumber) {
+		if (genre.contentEquals("전체")) {
+			genre = "";
+		}
+		ArrayList<EvaluationDTO> evaluationList = null;
+		String SQL = "";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			if (searchType.contentEquals("최신순")) {
+				SQL = "SELECT * FROM EVALUATION WHERE genre LIKE ? AND CONCAT(movieTitle, directorName, evaluationTitle, evaluationContent) Like" +
+						"? ORDER BY evaluationID DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+			} else if (searchType.contentEquals("추천순")) {
+				SQL = "SELECT * FROM EVALUATION WHERE genre LIKE ? AND CONCAT(movieTitle, directorName, evaluationTitle, evaluationContent) Like" +
+						"? ORDER BY likeCount DESC LIMIT " + pageNumber * 5 + ", " + pageNumber * 5 + 6;
+			}
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, "%" + genre + "%");
+			pstmt.setString(2, "%" + search + "%");
+			rs = pstmt.executeQuery();
+			evaluationList = new ArrayList<EvaluationDTO>();
+			
+			while(rs.next()) {
+				EvaluationDTO evaluation = new EvaluationDTO(
+					rs.getInt(1), 
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getString(5),
+					rs.getString(6),
+					rs.getString(7),
+					rs.getInt(8),
+					rs.getInt(9),
+					rs.getInt(10),
+					rs.getInt(11),
+					rs.getInt(12)
+				);
+				evaluationList.add(evaluation);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)	conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null)	pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				if (rs != null)	rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return evaluationList;
 	}
 }
