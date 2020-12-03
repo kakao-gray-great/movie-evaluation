c222a11c2fbf61902fa95e7586e4425f35043c3c
operasystem6 <operasystem6@naver.com>
2020-12-04 08:21:38 +0900

    feat: 유효성 검사 js 구현

--- WebContent/js/validation.js
+++ WebContent/js/validation.js
@@ -4,7 +4,6 @@ function CheckValue() {
 	var userPassword = document.getElementById("userPassword");
 	var userEmail = document.getElementById("userEmail");
 	var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-	var email = document.signup.userEmail.value;
 	
 	//아이디 유효성 검사 (영문소문자, 숫자만 허용)
 	for (var i = 0; i < userID.value.length; i++) {
@@ -24,8 +23,8 @@ function CheckValue() {
         return false;
     }
   	//아이디 길이 체크 (6~12자)
-    if (userID.value.length < 6 || userID.value.length > 12) {
-        alert("아이디를 6~12자까지 입력해주세요.")
+    if (userID.value.length < 4 || userID.value.length > 12) {
+        alert("아이디를 4~12자까지 입력해주세요.")
         userID.focus();
         userID.select();
         return false;
@@ -36,26 +35,17 @@ function CheckValue() {
         return false;
     }
   	//비밀번호 길이 체크(6~14자 까지 허용)
-    if (userPassword.value.length<6 || userPassword.value.length>14) {
+    if (userPassword.value.length < 6 || userPassword.value.length > 14) {
         alert("비밀번호를 6~14자까지 입력해주세요.")
         userPassword.focus();
-       userPassword.select();
+       	userPassword.select();
         return false;
     }
-    if (regex.test(userEmail) === false) {
+    if (regex.test(userEmail.value) === false) {
         alert("잘못된 이메일 형식입니다.");
         userEmail.value = ""
-       userEmail.focus();
+       	userEmail.focus();
         return false;
-    }
-    for (var i = 0; i < userEmail.value.length; i++) {
-        chm = userEmail.value.charAt(i)
-        if (!(chm >= '0' && chm <= '9') && !(chm >= 'a' && chm <= 'z')&&!(chm >= 'A' && chm <= 'Z')) {
-            alert("이메일은 영문 대소문자, 숫자만 입력가능합니다.")
-            userEmail.focus();
-            userEmail.select();
-            return false;
-        }
     }
 	document.signup.submit()
 }
